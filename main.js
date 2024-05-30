f4e71001cca8dc4e72ac02216f702edf4f0943ec
Ubuntu <ubuntu@ip-172-31-18-114.ec2.internal>
2024-05-30 09:11:07 +0000

    웹

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-const express=require('express');
+const express=require('express'); //module import
 const app=express() // express 실행
 
 app.listen(3000, ()=>{ // 서버 생성, 포트번호:3000
     console.log('서버 시작');
 }) 
+
+
+app.get('/',(req,res)=>{
+    console.log('루트에 대한 요청');
+    res.send('루트페이지에 대한 요청');
+}) 
+
+app.get('/test',(req,res)=>{
+    console.log('===> test에 대한 요청');
+    res.send('test에 대한 요청');
+}) //주소:3000/test치면 이 문단 뜸.
+
+app.get('/myaong',(req,res)=>{
+    console.log('===> mayong에 대한 요청');
+    res.send('아임 응애에요');
+})
