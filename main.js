4f6cbf94c43b98fa2f9866e016966ec743d075c8
myasehyun <qkrtpgus73@naver.com>
2024-06-05 09:17:25 +0000

    회원가입

--- main.js
+++ main.js
@@ -26,3 +26,7 @@ app.get('/login',(req,res)=>{
     console.log('로그인 요청');
     res.send('로그인 페이지 예시');
 }) 
+app.get('/register',(req,res)=>{
+    console.log('회원가입 요청');
+    res.send('회원가입');
+}) 
